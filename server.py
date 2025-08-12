3d08c5d496aa4a0a533e067f4f3a4416592cd844
Tarunno360 <sm.azmain.faysal@g.bracu.ac.bd>
2025-08-12 20:36:39 +0600

    server building basic

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+server_port=8080
+format = 'utf-8'
+buffer_for_message_length = 1024
+
+#create the servers address
+hostname= socket.gethostname()
+host_ip= socket.gethostbyname(hostname)
+addr= (host_ip,server_port)
+
+# create a socket for the client
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(addr)
+
+server.listen()  # Listen for incoming connections
+
+
