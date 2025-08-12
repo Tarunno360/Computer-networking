4645e8594700e4a17b922c4a08a5bbc98ba5aed1
Tarunno360 <sm.azmain.faysal@g.bracu.ac.bd>
2025-08-12 22:23:05 +0600

    completed task3

---
+++ Task3/client2.py
@@ -0,0 +1,31 @@
+import socket
+
+server_port=8080
+format = 'utf-8'
+buffer_for_message_length = 16
+
+#create the servers address
+hostname= socket.gethostname()
+host_ip= socket.gethostbyname(hostname)
+addr= (host_ip,server_port)
+
+# create a socket for the client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(addr)
+
+def message_to_be_sent(message):
+    msg= message.encode(format)
+    msg_length= str(len(message))
+    msg_length= msg_length.encode(format)
+    
+    padding=b" " * (buffer_for_message_length - len(msg_length))
+    msg_length+= padding
+    
+    client.send(msg_length)
+    client.send(msg)
+    
+    print(client.recv(2048).decode(format))
+    
+message_to_be_sent(f"Hi my name is Tarunno")
+message_to_be_sent("Disconnect")
