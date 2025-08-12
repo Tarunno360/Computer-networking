970846aa1908838937ed4323c54ac6147b851830
Tarunno360 <sm.azmain.faysal@g.bracu.ac.bd>
2025-08-12 20:52:35 +0600

    server ready

--- server.py
+++ server.py
@@ -13,10 +13,30 @@ addr= (host_ip,server_port)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(addr)
 
-server.listen()  # Listen for incoming connections
+server.listen() 
 
 while True:
     conn,add=server.accept()
     print(f"Connection established with {add}")
-      
+    
+    connected=True
+    while connected:
+        message_length= conn.recv(buffer_for_message_length).decode(format)
+        print("Upcoming message length:", message_length)
+        
+        if message_length:
+            message_length= int(message_length.strip())
+            message= conn.recv(message_length).decode(format)
+            
+            if message == "Disconnect":
+                connected = False
+                print("Terminating connection with ",add)
+                conn.send("The session is terminated".encode(format))
+                
+            else:
+                print(message)
+                conn.send("The server has received the message".encode(format))
+                print("\n")
+            
+conn.close()
     
